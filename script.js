d2f184472fb84cc7071f06cd67bbccb12e8e0c37
Kirsten-Tang <144397982+Kirsten-Tang@users.noreply.github.com>
2023-10-26 00:57:07 +0000

    Changed js structure, refined some css

--- script.js
+++ script.js
@@ -16,81 +16,62 @@ const Project = class {
     }
 };
 
-var projectArray = [
-    new Project(
-      "Social Learning Design",
-      "PawfectNYC: Connecting NYC's Dog Community",
-      "PawfectNYC is a social learning platform designed to support the learning needs of 20-35 NYC dog owners who are commuters. The platform aims to help dog owners balance their busy lifestyles with the needs of their beloved pets by providing a structured and supportive online community for learning and sharing information.",
-      "pawfectnyc.png",
-      "four iPhone screens that show the interfaces of Pawfectnyc"
-    ),
-    new Project(
-      "Learning Experience Design",
-      "Learning Solution Designed for 65+ Seniors",
-      "In this project, our team has created a series of in-person courses for 65+ seniors to gain more social connections and help them learn how to use common social media such as WeChat, Alipay, and YouTube. We also used Google Classroom to create course prototypes to make the physical courses easier for stakeholders to understand in a more visual form.",
-      "seniors.jpg",
-      "four seniors sitting on the ground learning on their phones"
-    ),
-    new Project(
-        "Learning Design Analysis",
-        "Learning Design Case Study: Kahoot!",
-        "In this project, I chose Kahoot as the existing learning design for analysis, such as its learners, motivation, learning context, learning experience, learning goals, learning theories, design process, and overall design critique.",
-        "kahoot.png",
-        "kahoot characters with a slogan:Make Learning Awesome"
-    ),
-    new Project(
-        "Narrative Design",
-        "Narrative Design for Learning Projects",
-        "In the Narrative course by Professor Ben Maddox, we have designed and produced a series of learning theory and science-based narrative pieces using a variety of digital media formats, namely Narrative in Audio, Games, Social Media & Learning, VR, MR & AR, and Motion Graphics.",
-        "narrative.jpg",
-        "A vintage printer with the words stories matter on the paper"
-    ),
-    new Project(
-        "UX design",
-        "NYU Course Registration: Albert",
-        "ALBERT, NYU's registration system, works seamlessly for its basic function but leaves a lot to be desired when helping students decide what to take or find courses across the many different schools and programs. In this project, we investigated student needs, frustrations, and wants regarding course selection to identify key insights pointing to how we might design better course selection resources and tools.",
-        "albert.jpg",
-        "A laptop with newly designed Albert interface"
-    ),
-    new Project(
-        "Learning Science Research",
-        "FOLS Final Report: Learning on Reddit",
-        "Established in 2005, Reddit is a 17-year-old social platform that allows users to discover and share content matching their interests. Stephanie, Shihan and I each selected an observation lens to explore learning behaviors on Reddit. I also made a Wix site to showcase our findings and analysis.",
-        "reddit.jpg",
-        "A vintage printer with the words stories matter on the paper"
-    ),
-];
+const projectGridElement = document.querySelector('.project-grid');
+let globalData;
 
-var projectContent = "";
-projectArray.forEach(function(project) {
-    projectContent += `
-      <article>
-        <img src="assets/${project.imageSrc}" alt="${project.imageAlt}">
-        <h3>${project.header}</h3>
-        <p>${project.description}</p>
-      </article>
-    `;
-});
+fetch("projects.json")
+    .then((response) => response.json())
+    .then((data) => {
+        globalData = data;
+        createContent(globalData);
+    });
 
-var projectGridElement = document.querySelector('.project-grid');
-console.log(projectGridElement);
-projectGridElement.innerHTML = projectContent;
+function createContent(globalData){
+    globalData.forEach(function(item){
+        buildElements(item);
+    });
+};
+
+function buildElements(item){
+    var articleElement = document.createElement("article");
+    articleElement.setAttribute('id', item.id);
+
+    var imgElement = document.createElement("img");
+    imgElement.setAttribute('src', `assets/${item.imageSrc}`);
+    imgElement.setAttribute('alt', item.imageAlt);
+    articleElement.appendChild(imgElement);
+
+    var h3Element = document.createElement("h3");
+    h3Element.textContent = item.header;
+    articleElement.appendChild(h3Element);
+
+    var pElement = document.createElement("p");
+    pElement.textContent = item.description;
+    pElement.classList.add('project-description');
+    articleElement.appendChild(pElement);
+
+    projectGridElement.append(articleElement);
+
+    articleElement.addEventListener('click', function (event) {
+      this.classList.add('clicked');
+      });
+};
 
 function hideAllSections() {
     document.querySelectorAll('main > section').forEach(function (section) {
       section.style.display = 'none';
     });
-  }
-  
-  function showSection(sectionClass) {
+}
+
+function showSection(sectionClass) {
     hideAllSections();
     const section = document.querySelector(`.${sectionClass}`);
     if (section) {
       section.style.display = 'grid';
     }
-  }
-  
-  document.querySelector('.menu-bar').addEventListener('click', function (event) {
+}
+
+document.querySelector('.menu-bar').addEventListener('click', function (event) {
     const text = event.target.textContent;
     if (text === 'About') {
       showSection('landing');
@@ -101,6 +82,10 @@ function hideAllSections() {
     } else if (text === 'Contact') {
       showSection('contact');
     }
-  });
-  
-  showSection('landing');
+    document.querySelectorAll('.menu-bar li').forEach(function (li) {
+      li.classList.remove('clicked');
+    });
+    event.target.classList.add('clicked');
+});
+
+showSection('landing');
